52be6953fc5c66c6e68cb69328bfd74e8626675c
deimelperez <deimelperez@gmail.com>
2021-03-03 11:39:03 +0100

    Simple websacraping

--- BasketbalData/basket_EDA.py
+++ BasketbalData/basket_EDA.py
@@ -1,8 +1,36 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-from matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import base64
 
 st.title('NBA Players Stats Explorer')
+
+st.write("""
+This app performs simple webscraping of NBA player stats data!
+
+""")
+
+st.sidebar.header('User Input Features')
+selecte_year = st.sidebar.selectbox('Year', list(reversed(range(1950, 2022))))
+
+# Web scraping
+
+
+@st.cache
+def load_data(year):
+    url = 'https://www.basketball-reference.com/leagues/NBA_' + \
+        str(year) + '_per_game.html'
+    html = pd.read_html(url, header=0)
+    df = html[0]
+    df
+    # Deletes repeating headers in content
+    raw = df.drop(df[df.Age == 'Age'].index)
+    raw = raw.fillna(0)
+    playerStats = raw.drop(['Rk'], axis=1)
+    return playerStats
+
+
+playerStats = load_data(selecte_year)
+playerStats
