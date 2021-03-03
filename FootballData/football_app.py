96a4337356b33a8246fad0941d8ef3985fef8196
deimelperez <deimelperez@gmail.com>
2021-03-03 16:32:19 +0100

    Football web scraping

--- FootballData/football_app.py
+++ FootballData/football_app.py
@@ -4,3 +4,36 @@ import base64
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+
+
+st.title('NFL Football Stats (Rushing) Explorer')
+
+st.markdown("""
+This app performs simple webscraping of NFL Football player stats data (focusing on Rushing)!
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Data source:** [pro-football-reference.com](https://www.pro-football-reference.com/).
+""")
+
+
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1990, 2020))))
+
+# Web scraping of NFL player stats
+# https://www.pro-football-reference.com/years/2019/rushing.htm
+
+
+@st.cache
+def load_data(year):
+    url = "https://www.pro-football-reference.com/years/" + \
+        str(year) + "/rushing.htm"
+    html = pd.read_html(url, header=1)
+    df = html[0]
+    # Deletes repeating headers in content
+    raw = df.drop(df[df.Age == 'Age'].index)
+    raw = raw.fillna(0)
+    playerstats = raw.drop(['Rk'], axis=1)
+    return playerstats
+
+
+playerstats = load_data(selected_year)
+playerstats
