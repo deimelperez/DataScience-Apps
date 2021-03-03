41246712bffbfa9d6c823c87b4e479085ec8d4ff
deimelperez <deimelperez@gmail.com>
2021-03-03 17:03:04 +0100

    Web scrapping S&P500 in wikipedia

--- S&P500/s&p500_app.py
+++ S&P500/s&p500_app.py
@@ -5,3 +5,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import yfinance as yf
+
+
+st.title('S&P 500 App')
+
+st.markdown("""
+This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+""")
+st.sidebar.header('User Input Features')
+
+
+# Web scraping of S&P 500 data
+@st.cache
+def load_data():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    html = pd.read_html(url, header=0)
+    df = html[0]
+    return df
+
+
+df = load_data()
+sector = df.groupby('GICS Sector')
